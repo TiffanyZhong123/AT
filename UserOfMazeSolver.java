418ff57ab59d8fd38f3c5ae644be679c8c243832
achopey <34114325+achopey@users.noreply.github.com>
2018-04-13 13:19:16 -0400

    Add files via upload

---
+++ UserOfMazeSolver.java
@@ -0,0 +1,25 @@
+//generates the test mazes which will test the base case, minimum recrusive abstraction case, and a larger case
+public class UserOfMazeSolver {
+	private static Displayer displayer;
+
+  public static void main( String[] commandLine)
+
+	throws java.io.FileNotFoundException {
+
+  	Maze maze = new Maze( commandLine[0],
+			     Integer.parseInt( commandLine[1]),
+			     Integer.parseInt( commandLine[2])
+			      );
+    int Length =  Integer.parseInt(commandLine[3]);
+
+    baseCase (maze, Length);
+  }
+
+private static void baseCase (Maze maze, int displayLength){
+
+      MazeSolver.display(displayLength);
+
+      System.out.println(MazeSolver.solver(maze));
+
+    }
+	}
